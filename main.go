a7372c078bbe54ee8bfa2ae9e88f5f3a4f0e8994
davitIV <davit.meshveliani.1@iliauni.edu.ge>
2025-03-09 23:28:53 +0400

    Initial commit

--- main.go
+++ main.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
 
+	// მთავარი პუნქტი
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, Fiber! 🚀")
+		return c.SendString(`
+			<!DOCTYPE html>
+			<html lang="ka">
+			<head>
+				<meta charset="UTF-8">
+				<title>ფერების შეცვლა</title>
+				<style>
+					body {
+						background-color: #f0f0f0;
+					}
+					h1 {
+						color: red; /* აქ შეგიძლიათ შეცვალოთ ფერი */
+					}
+				</style>
+			</head>
+			<body>
+				<h1>გამარჯობა სიდო 🚀</h1>
+			</body>
+			</html>
+		`)
 	})
 
 	fmt.Println("Server is running on http://localhost:3000")
