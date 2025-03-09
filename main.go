bc8eac335dfd0f833547f8b76c072bdb6cd00c4b
davitIV <davit.meshveliani.1@iliauni.edu.ge>
2025-03-09 22:59:57 +0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, Fiber! 🚀")
+	})
+
+	fmt.Println("Server is running on http://localhost:3000")
+	app.Listen(":3000")
+}
