fc2013d437fc6bc946d79b1a0e36dc0e792b9099
davitIV <davit.meshveliani.1@iliauni.edu.ge>
2025-03-11 00:18:05 +0400

    Initial commit

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
@@ -14,8 +15,15 @@ var db *pgxpool.Pool
 func main() {
 	// PostgreSQL კავშირის შექმნა
 	databaseUrl := os.Getenv("DATABASE_URL") // Railway-სგან მიღებული URL
+	if databaseUrl == "" {
+		log.Fatal("DATABASE_URL environment variable is not set")
+	}
+
+	// context.Context-ი საჭიროა PostgreSQL კავშირის დასამყარებლად
+	ctx := context.Background()
+
 	var err error
-	db, err = pgxpool.New(nil, databaseUrl)
+	db, err = pgxpool.New(ctx, databaseUrl)
 	if err != nil {
 		log.Fatal("DB Connection Error: ", err)
 	}
@@ -28,6 +36,7 @@ func main() {
 		return c.SendString("Hello, World!")
 	})
 
+	// კავშირის წარმატებით დამყარების შეტყობინება
 	fmt.Println("Server is running on http://localhost:3000")
 	app.Listen(":3000")
 }
